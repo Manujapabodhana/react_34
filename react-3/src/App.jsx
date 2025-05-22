331385040dbca9ca846d45552ee5011d7b7a0858
manuja <rpmanuja123@gmail.com>
2025-05-22 15:30:01 +0530

    hh

--- react-3/src/App.jsx
+++ react-3/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderContent from './Components/HeaderContent/HeaderContent';
+import Bodycontent from './components/Bodycontent/Bodycontent';
 
 function App() {
   return (
     <div id="wrapper">
       <HeaderContent />
+      <Bodycontent></Bodycontent>
     </div>
   );
 }
---
+++ react-3/src/components/Bodycontent/Bodycontent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Bodycontent.css'; // fixed the path (no space, correct case)
+
+function Bodycontent() {
+  return (
+    <div className="content"> {/* fixed className spelling */}
+      <h3>Bodycontent</h3>
+    </div>
+  );
+}
+
+export default Bodycontent;
